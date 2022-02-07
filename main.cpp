159649f2961fd4e4db6b5e4f7d0398674f9a79fb
thierrygomes <thierry.gomes@estudante.ufla.br>
2022-02-07 16:40:37 -0300

    Menu switch case rascunho
    rascunho menu inicial do trabalho

--- main.cpp
+++ main.cpp
@@ -4,9 +4,67 @@
 
 using namespace std;
 
+void menu(){
+  bool sair = false;
+  
+  while (sair != true){
+    
+    cout <<"**********MENU PRINCIPAL**********" << endl << endl;
+  
+    cout <<"(1)............Deseja ler/abrir um arquivo"<<endl;
+    cout <<"(2)............Incluir elemento" << endl;
+    cout <<"(3)............Visualizar o registro entre posicao dos elementos" << endl;
+    cout <<"(4)............Alterar dados do registro entre posicao dos elementos" << endl;
+    cout <<"(5)............Imprimir Dados" << endl;
+    cout <<"(6)............Sair" << endl << endl;
+    cout <<"**********************************" << endl<< endl;
+    
+  
+    cout << "Digite o numero da opcao desejada:";
+    
+    char opcao;
+    cin >> opcao;
+    
+    
+    switch(opcao){
+     /* case"1":
+      
+      break;
+      
+      case"2":
+      
+      break;
+      
+      case"3":
+      
+      break;
+      
+      case"4":
+      
+      break;
+      
+      case"5":
+      
+      break;
+      */
+      
+      case '6':
+      sair = true;
+      
+      break;
+      
+    default:
+      cout << "opcao invalida." << endl;
+  }
+  
+}
+}
+
 
 int main(int argc, char const *argv[])
 {
+    menu();
+    
     sanFrancisco reader;
     ifstream arquivo;
     arquivo.open("./data/san_francisco_payroll_dataset.csv");
